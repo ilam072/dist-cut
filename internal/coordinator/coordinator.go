647a305410588c31c6fe0a5088b4466bf17ca63b
Adam Elmurzaev <adamibnkhavaj@gmail.com>
2026-03-02 17:02:02 +0500

    reuse http.Client and fix response body leak

--- internal/coordinator/coordinator.go
+++ internal/coordinator/coordinator.go
@@ -38,12 +38,18 @@ func Main(peers []string, listenAddr string, fields, delim string, replication, 
 		}
 	}
 
+	client := &http.Client{
+		Timeout: 10 * time.Second,
+	}
+
 	type result struct {
 		idx int
 		res string
 		err error
 	}
+
 	results := make([]string, shardCount)
+
 	for i := 0; i < shardCount; i++ {
 
 		repNodes := make([]string, 0, replication)
@@ -53,62 +59,88 @@ func Main(peers []string, listenAddr string, fields, delim string, replication, 
 
 		ch := make(chan result, len(repNodes))
 		var wg sync.WaitGroup
+
 		for _, node := range repNodes {
 			wg.Add(1)
+
 			go func(node string, idx int) {
 				defer wg.Done()
-				if node == listenAddr {
 
+				if node == listenAddr {
 					res, err := worker.ProcessChunk(shards[idx], fields, delim)
 					ch <- result{idx: idx, res: res, err: err}
 					return
 				}
 
-				tr := worker.TaskRequest{Chunk: shards[idx], Fields: fields, Delim: delim}
-				jb, _ := json.Marshal(tr)
-				client := &http.Client{Timeout: 10 * time.Second}
+				tr := worker.TaskRequest{
+					Chunk:  shards[idx],
+					Fields: fields,
+					Delim:  delim,
+				}
+
+				jb, err := json.Marshal(tr)
+				if err != nil {
+					ch <- result{idx: idx, err: err}
+					return
+				}
+
 				url := fmt.Sprintf("http://%s/process", node)
 				resp, err := client.Post(url, "application/json", bytes.NewReader(jb))
 				if err != nil {
 					ch <- result{idx: idx, err: err}
 					return
 				}
-				b, _ := io.ReadAll(resp.Body)
+				defer resp.Body.Close()
+
+				b, err := io.ReadAll(resp.Body)
+				if err != nil {
+					ch <- result{idx: idx, err: err}
+					return
+				}
+
 				var tres worker.TaskResponse
 				if err := json.Unmarshal(b, &tres); err != nil {
 					ch <- result{idx: idx, err: err}
 					return
 				}
+
 				if tres.Err != "" {
 					ch <- result{idx: idx, err: fmt.Errorf(tres.Err)}
 					return
 				}
-				ch <- result{idx: idx, res: tres.Result, err: nil}
+
+				ch <- result{idx: idx, res: tres.Result}
 			}(node, i)
 		}
 
-		go func() { wg.Wait(); close(ch) }()
+		go func() {
+			wg.Wait()
+			close(ch)
+		}()
 
 		successes := 0
 		var chosen string
 		errs := make([]string, 0)
+
 		for r := range ch {
 			if r.err == nil {
 				successes++
-
 				if chosen == "" {
 					chosen = r.res
 				}
 			} else {
 				errs = append(errs, r.err.Error())
 			}
+
 			if successes >= quorum {
 				break
 			}
 		}
+
 		if successes < quorum {
 			return fmt.Errorf("shard %d failed quorum: got %d, errs: %v", i, successes, errs)
 		}
+
 		results[i] = chosen
 	}
 
